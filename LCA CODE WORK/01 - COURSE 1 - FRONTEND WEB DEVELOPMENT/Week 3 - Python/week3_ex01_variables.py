1762dd56961ffc9ce9edd11cdc927b179c73b240
Shuaib Darries <shuaibdarries@gmail.com>
2026-04-21 16:09:07 +0200

    Completed Python Exercise 01: Introduction and Variables

--- LCA CODE WORK/01 - COURSE 1 - FRONTEND WEB DEVELOPMENT/Week 3 - Python/week3_ex01_variables.py
+++ LCA CODE WORK/01 - COURSE 1 - FRONTEND WEB DEVELOPMENT/Week 3 - Python/week3_ex01_variables.py
@@ -1,2 +1,20 @@
+#Question 1 Variable Assignment and String Manipulation
+
 name = input("Enter your name: ")
-print("Hello " + name + "!")   
+age = input("Enter your age: ")
+print(f"hello, {name}! You are {age} years old.")
+
+
+#Question 2 Integer Operations
+
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+area = length * width
+print(f"The area of the rectangle is: {area}")
+
+
+#Question 3: Working with Floats
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {fahrenheit:.2f}")
